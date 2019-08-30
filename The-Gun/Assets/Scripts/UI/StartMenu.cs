bc1f4040e76f3b601abb9b1da8cc49d5616fee45
gerh667 <gerh667@gmail.com>
2019-08-30 23:54:54 +0300

    Add start menu

---
+++ The-Gun/Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Button btnStart;
+    [SerializeField]
+    private Button btnQuit;
+
+    private void OnEnable()
+    {
+        btnStart.onClick.AddListener(OnBtnStartCkick);
+        btnQuit.onClick.AddListener(OnBtnQuitClick);
+    }
+
+    private void OnBtnStartCkick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    private void OnBtnQuitClick()
+    {
+        Application.Quit();
+    }
+
+    private void OnDisable()
+    {
+        btnStart.onClick.RemoveListener(OnBtnStartCkick);
+        btnQuit.onClick.RemoveListener(OnBtnQuitClick);
+    }
+}
