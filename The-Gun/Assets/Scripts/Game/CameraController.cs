ff97f026e2cd70b5326ca0012d0048e444b8beb9
gerh667 <gerh667@gmail.com>
2019-08-31 00:28:13 +0300

    Add player controller and camera controller

---
+++ The-Gun/Assets/Scripts/Game/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    PlayerController player;
+
+    Vector3 offset;
+    Vector3 centerPos;
+
+    float distance;
+
+    int floorMask;
+
+    [Header("Camera settings")]
+    [SerializeField] [Range(1f, 20f)] float range = 10f;
+    [SerializeField] [Range(0.01f, 0.5f)] float sensetive = 0.25f;
+
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        offset = transform.position - player.transform.position;
+        floorMask = LayerMask.GetMask("Floor");
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetButton("Fire3"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // todo make attach to floor mask
+
+            if (Physics.Raycast(ray, out hit, 100, floorMask))
+            {
+                centerPos = new Vector3(hit.point.x - player.transform.position.x, 0, hit.point.z - player.transform.position.z);
+                centerPos.Normalize();
+                distance = Vector3.Distance(player.transform.position, hit.point) * sensetive;
+                distance = Mathf.Clamp(distance, 0f, range);
+            }
+        }
+        else
+        {
+            distance = 0f;
+        }
+        Vector3 newCamPos = player.transform.position + centerPos * distance + offset;
+        transform.position = newCamPos;
+    }
+}
---
+++ The-Gun/Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] [Range(0.01f, 10f)] float moveSpeed = 5f;
+
+    [Header("Dash")]
+    [SerializeField] [Range(0.01f, 10f)] float dashTime = 0.5f;
+    [SerializeField] [Range(0.01f, 10f)] float dashForce = 3f;
+
+    int floorMask;
+
+    bool isDashing = false;
+
+    Vector2 sumMove;
+
+    Vector3 lookPos;
+
+    Animation swordHit;
+
+    ParticleSystem gunParticleSystem;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        gunParticleSystem = transform.GetComponentInChildren<ParticleSystem>();
+        swordHit = transform.GetComponentInChildren<Animation>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(0, 0, 0);
+        ProcessTranslation();   // WASD moving
+        ProcessRotation();  // look at cursor
+        //ProcessHitting();   // melee attack
+        //ProcessShooting();  // range attack
+        StartCoroutine(ProcessDash());  // boosting if 'Jump' pressed
+
+    }
+
+    private void ProcessTranslation()
+    {
+        // get input
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        // creating normalized(it means that diagonal movement NOT faster) movement vector2 
+        if (!isDashing)
+        {
+            sumMove = new Vector2(xMove, zMove);
+        }
+        sumMove.Normalize();
+        sumMove = sumMove * moveSpeed;
+
+        // calculating translation
+        float newXPos = transform.position.x + sumMove.x * Time.deltaTime;
+        float newZPos = transform.position.z + sumMove.y * Time.deltaTime;
+
+        // applying translition
+        transform.position = new Vector3(newXPos, transform.position.y, newZPos);
+    }
+
+    private void ProcessRotation()
+    {
+        // make player look at raycast hit point Vector3 (without 'y' component)
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100, floorMask))
+        {
+            lookPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            transform.LookAt(lookPos);
+        }
+    }
+
+    private void ProcessHitting()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            swordHit.Play();
+        }
+    }
+
+    private void ProcessShooting()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            gunParticleSystem.Play();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            gunParticleSystem.Stop();
+        }
+    }
+
+    IEnumerator ProcessDash()
+    {
+
+        if (Input.GetButtonDown("Jump") && !isDashing)
+        {
+            if (sumMove != Vector2.zero) // if player is moving then dashing at movement direction
+            {
+                isDashing = true;
+                moveSpeed = moveSpeed * dashForce;
+                yield return new WaitForSeconds(dashTime);
+                moveSpeed = moveSpeed / dashForce;
+                isDashing = false;
+            }
+            else if (sumMove == Vector2.zero) // if player doesn't move at this moment then dashing at mouse position
+            {
+                sumMove = new Vector2(lookPos.x - transform.position.x, lookPos.z - transform.position.z);
+                sumMove.Normalize();
+                isDashing = true;
+                moveSpeed = moveSpeed * dashForce;
+                yield return new WaitForSeconds(dashTime);
+                moveSpeed = moveSpeed / dashForce;
+                isDashing = false;
+            }
+        }
+    }
+}
