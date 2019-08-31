094c971e1911a89bd52ecf5e517eeca8906575ee
gerh667 <gerh667@gmail.com>
2019-08-31 14:34:56 +0300

    Setting new Player and Enemy

---
+++ The-Gun/Assets/Scripts/Game/RigidbodyEnemyTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyEnemyTrigger : MonoBehaviour
+{
+    private bool m_isActivated;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_isActivated) return;
+        if (other.gameObject.GetComponent<BulletProp>())
+        {
+            foreach (Transform child in GetComponentsInChildren<Transform>())
+            {
+                if (child != transform)
+                {
+                    child.gameObject.AddComponent<Rigidbody>();
+                }
+                m_isActivated = true;
+            }
+        }
+    }
+}
